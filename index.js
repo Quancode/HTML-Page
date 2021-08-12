9a4fbc2d1ab9331138db639bbadd3fa21504c36d
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-12 13:11:47 +0200

    Opdracht 1 Module 1 JS
    Opdracht 1 Module 1 JS

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('Hello Winc Academy');
+
+let name = "Quanita";
+console.log(name);
+
+/*variabele IF kan niet worden gebruikt omdat dit al een waarde is in JS*/
+
+/*in console kan je de nummers 4+4 bij elkaar optellen.
+Het telt de cijfers bij elkaar op =8 */
+let number = 4 + 4;
+console.log(number);
+
+
+/*in console kan je 2 strings "4"+"4" bij elkaar optellen.
+het zet de cijfers 4 en 4 naast elkaar="44"*/
+
+let addition = "4" + "4";
+console.log(addition)
+
+console.log(10 - 4);
+console.log(4 * 4);
+console.log(4 / 4);
+console.log(4 % 4);
+
+let age = "39";
+console.log(age)
+
+let text = "thirtynine";
+console.log(text)
+
+
+
