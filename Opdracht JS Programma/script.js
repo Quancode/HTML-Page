761636b6ad8da393d631e71c7f1ad8912cb657a3
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-13 14:31:21 +0200

    JavaScript met Nodejs
    js JavaScript met Nodejs

---
+++ Opdracht JS Programma/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+let multiplication = (1230941 * 1823794);
+console.log("multiplication:", multiplication);
+
+let division = (637263 / 54);
+console.log("division:", division);
+
+/* ik zie bovenstaande uitkomsten in mijn console
+omdat ik de script.js run met node*/
+
+/*als ik de rekensommen in een comment zet komen ze niet node.
+als er 'niets' in script.js staat, runt er ook niets in de console.
+En als er niets in console runt, runt er ook niet in de node.*/
