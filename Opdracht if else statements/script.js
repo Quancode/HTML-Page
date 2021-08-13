fe5db48fa346736ecc34da58797810e399d356d6
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-13 16:27:15 +0200

    If Else Statements
    If Else Statements

---
+++ Opdracht if else statements/script.js
@@ -0,0 +1,52 @@
+console.log("Hallo Iedereen");
+
+/*const age = 9;
+
+if (age > 18) {
+
+    document.write("Je mag naar binnen");
+
+    console.log(age)
+}
+
+else if (age > 18) {
+
+    document.write("Je mag niet naar binnen. U word vriendelijk verzocht buiten te blijven")
+
+    console.log(age)
+}
+
+else if (age > 10) {
+
+    document.write("Je mag buiten een ijsje");
+
+    console.log(age)
+}
+
+else if (age > 5) {
+
+    document.write("Go home");
+
+    console.log(age)
+}*/
+
+/*
+const isFemale = true; 
+
+if (isFemale) {
+
+    document.write("Kom binnen");
+
+    console.log(isFemale)
+}
+*/
+
+const driverStatus = "bob";
+
+if (driverStatus === "bob") { //status is gelijk
+    console.log("Jij bent de bob")
+}
+
+else {
+    console.log("Jij mag niet rijden")
+}
