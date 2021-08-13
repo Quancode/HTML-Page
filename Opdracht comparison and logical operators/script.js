715982213582c0d9e2cda5d9e12fa1301f842af2
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-13 18:05:32 +0200

    js comparison and logical operators
    js comparison and logical operators

---
+++ Opdracht comparison and logical operators/script.js
@@ -0,0 +1,86 @@
+/*
+const age = 9;
+
+if (age > 18) {
+
+    document.write("Je mag naar binnen");
+
+    console.log(age)
+}
+
+else if (age > 18) {
+
+    document.write("Je mag niet naar binnen. U word vriendelijk verzocht buiten te blijven")
+
+    console.log(age)
+}
+
+else if (age > 10) {
+
+    document.write("Je mag buiten een ijsje");
+
+    console.log(age)
+}
+
+else if (age > 5) {
+
+    document.write("Go home");
+
+    console.log(age)
+}
+
+
+const isFemale = true;
+
+if (isFemale) {
+
+    document.write("Kom binnen");
+
+    console.log(isFemale)
+}
+
+const driverStatus = "bob";
+
+if (driverStatus === "no") { //waarde is gelijk aan
+    console.log("Jij bent de bob")
+}
+
+else {
+    console.log("Jij mag niet rijden")
+}
+*/
+//---------------------------------------------------
+
+const age = 14;
+
+if (age >= 18 && age <= 25) {
+
+    console.log("Je krijgt 50% korting!");
+} else {
+    console.log("Je krijgt geen korting");
+}
+
+
+const firstName = "ben";
+
+if (firstName === "sarah" || firstName === "bram") {
+
+    console.log("Jij krijgt een gratis biertje!");
+} else {
+    console.log("Je krijgt sap");
+}
+
+
+const totalAmount = 26;
+
+if (totalAmount >= 100) {
+
+    console.log("Je krijgt  een gratis flesje champagne");
+}
+else if (totalAmount > 50) {
+
+    console.log("Je krijgt een gratis portie nachos");
+} else if (totalAmount > 25) {
+
+    console.log("Je krijgt gratis (vega)bitterballen");
+}
