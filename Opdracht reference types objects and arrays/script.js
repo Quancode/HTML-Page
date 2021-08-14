8a83bcf19f5d996b1772587693340473d5e2bce9
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-14 14:07:20 +0200

    objects and arrays
    objects and arrays

--- Opdracht reference types objects and arrays/script.js
+++ Opdracht reference types objects and arrays/script.js
@@ -67,31 +67,54 @@ else if (totalAmount > 50) {
 let person = {
     name: "Quanita",
     age: 38,
-    evaluations: [7,10,9],
+    evaluations: [7, 10, 9],
 };
 /*console.log(person.evaluations);*/
 // de waarde hiervan is 3
 
+// entire object
+console.log(person);
+
+// dot notation
+console.log(person.name);
+console.log(person.age);
+
+// bracket notation
+console.log(person["name"]);
+console.log(person["age"]);
+
+// extra key-value pair
+console.log(person.evaluations);
 
 //-----------Arrays-------------------
 
-var colors = [ 
-    "groen", 
-    "blauw", 
+const colors = [
+    "groen",
+    "blauw",
     "rood"];   // aray met strings
 
+console.log(colors);
+console.log(colors.length);
 
-colors[3] = "3";                        
-
-let count = colors.push("geel");             // aray.push
-let number = colors.push("5");  
-let greeting = colors.push("greetings: Hi ik ben een object");
-
-
-/*console.log(colors.length);
+// first element
 console.log(colors[0]);
-console.log(colors[2]);*/
-console.log(colors[6]);
+
+// last element
+console.log(colors[colors.length - 1]);
+
+// adding elements
+colors.push("geel");
+console.log(colors);
+colors.push(5);
+console.log(colors);
+
+// adding an object
+colors.push({ greeting: "Hi ik ben een object" });
+console.log(colors);
+
+// only the last greeting
+console.log(colors[colors.length - 1].greeting);
+
 
 
 //-----------Bekijk een real-life object-------------------
@@ -105,43 +128,56 @@ const catBreeds = [{
     origin: "Egypt",
     temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"], //--aray
     wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
-                food: {
-                        favourite_food: "fish",
-      medium_liked_food: "dried fruits", 
-      disliked_food: "walnuts"
-    
-     }
-  },
-        {
+    food: {
+        favourite_food: "fish",
+        medium_liked_food: "dried fruits",
+        disliked_food: "walnuts"
+
+    }
+},
+{
     name: "Aegean",
     description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
     dog_friendly: 4,
     energy_level: 53,
     life_span: "9- 12",
     origin: "Greece",
-    temperament: [ "Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    temperament: ["Affectionate", "Social", "Intelligent", "Playful", "Active"],
     wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
     food: {
-                        favourite_food: "tuna",
-      medium_liked_food: "canned food", 
-      disliked_food: "all fruits"
-     }
-  },
-        {
+        favourite_food: "tuna",
+        medium_liked_food: "canned food",
+        disliked_food: "all fruits"
+    }
+},
+{
     name: "American Bobtail",
     description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
     dog_friendly: 5,
     energy_level: 3,
     life_span: "11 - 15",
     origin: "United States",
-    temperament: [ "Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    temperament: ["Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
     wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
     food: {
-                        favourite_food: "meaty things",
-      medium_liked_food: "tuna", 
-      disliked_food: "canned food"
-    } 
-  }
+        favourite_food: "meaty things",
+        medium_liked_food: "tuna",
+        disliked_food: "canned food"
+    }
+}
 ]
-console.log(catBreeds.length); 
-console.log(catBreeds);
+
+
+console.log("Naam kattensoort 3:", catBreeds[2].name);
+console.log("Energylevel kattensoort 1:", catBreeds[0].energy_level);
+console.log("Eerste temperament kat 2:", catBreeds[1].temperament[0]);
+
+const lengthTemperamentsCatThree = catBreeds[2].temperament.length;
+console.log(
+    "Laatste temperament kat 3:",
+    catBreeds[2].temperament[lengthTemperamentsCatThree - 1]
+);
+
+console.log("Favo voedsel van kat 3:", catBreeds[2].food.favourite_food);
+
+
