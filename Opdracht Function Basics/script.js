99672dbfaf20942cfceb72392399a85c226582cd
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-14 14:45:18 +0200

    to call a function - basics
    to call a function - basics

---
+++ Opdracht Function Basics/script.js
@@ -0,0 +1,30 @@
+console.log("Hier ben ik weer");
+
+
+//steps in function for Pizza
+const pizzaMaker = function () {
+    console.log("voorverwarm oven");
+    console.log("bekleed bakplaat met bakpapier");
+    console.log("rol deeg uit over bakplaat");
+    console.log("smeer deeg in met tomatensaus");
+    console.log("verdeel kaas over piza");
+    console.log("pizza in oven");
+};
+
+
+//steps in function for sushi
+const sushiMaker = function () {
+    console.log("wassen rijst");
+    console.log("mengsel rijst, rijstazijn, wijnazijn, suiker en zout in magnetron");
+    console.log("rijst in pan met water koken");
+    console.log("rijst afgieten in houten schaal en rijst laten afkoelen");
+    console.log("bedek de sushirijst met een theedoek");
+    console.log("Leg het nori vel met de gladde kant naar de onderkant en zorg dat je het vel met de sushirijst goed bedekt");
+    console.log("ruimte van ongeveer 1 cm vrij laten");
+};
+//call the function, I am getting the result that I want. 
+sushiMaker();
+sushiMaker();
+sushiMaker();
+pizzaMaker();
+pizzaMaker();
