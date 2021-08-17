aa2bc621dbb8733f2d491e5fb8cd4e67ae3115c0
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-17 15:20:14 +0200

    Function return statements
    Function return statements

--- Opdracht Function return statements/script.js
+++ Opdracht Function return statements/script.js
@@ -1,19 +1,112 @@
+/*--------checking if a number is big--------------*/
 
-
-const number = function (myAge) {
-    console.log(myAge);
-    return number;
-}
-
-const number = function (myAge) {
-    if (myAge >= 100) {
-        console.log("true");
+const numberIsBig = function (number) {
+    if (number > 100) {
+        return true;
     } else {
-        console.log("false");
+        return false;
     }
 };
 
-const result = myAge(55);
-console.log(result);
+console.log(numberIsBig(150));
+console.log(numberIsBig(50));
+console.log(numberIsBig(0));
+console.log(numberIsBig(999));
 
-//--------
+//This is a function that produces something. It does not use console.log.
+
+//Because number < 100 already returns true or false we can immediately return that too to make the function shorter:
+
+const numberIsBig = function (number) {
+    return number > 100;
+};
+
+/*--------bounder at a club--------------*/
+
+const getBouncerReaction = function (
+    maxVisitors,
+    currentVisitors,
+    ageOfPotentialVisitor
+) {
+    if (ageOfPotentialVisitor < 18) {
+        return "this is a club for adults";
+    }
+
+    if (currentVisitors >= maxVisitors) {
+        return "it's too busy now, come back later";
+    } else {
+        return "come in";
+    }
+};
+
+console.log(getBouncerReaction(2000, 0, 15)); // "this is a club for adults"
+console.log(getBouncerReaction(2000, 1999, 50)); // "come in"
+console.log(getBouncerReaction(2000, 2000, 40)); // "it's too busy now, come back later"
+console.log(getBouncerReaction(2000, 2999, 30)); // "it's too busy now, come back later"
+
+//--solution 2-- It's possible to skip the curly braces for an if statement if you only want one statement to be run after the if. 
+//Be careful with this as it can make your code less readable.
+
+const getBouncerReaction = function (
+    maxVisitors,
+    currentVisitors,
+    ageOfPotentialVisitor
+) {
+    if (ageOfPotentialVisitor < 18) return "this is a club for adults";
+    if (currentVisitors >= maxVisitors)
+        return "it's too busy now, come back later";
+    return "come in";
+};
+
+//--solution 3-- We can also use a temporary response variable, put a value into it and then return that variable at the end of the function. 
+//This can sometimes be a better pattern.
+
+const getBouncerReaction = function (
+    maxVisitors,
+    currentVisitors,
+    ageOfPotentialVisitor
+) {
+    let response;
+    if (ageOfPotentialVisitor < 18) {
+        response = "this is a club for adults";
+    } else if (currentVisitors >= maxVisitors) {
+        response = "it's too busy now, come back later";
+    } else {
+        response = "come in";
+    }
+    return response;
+};
+
+
+/*------Calculating the average--------*/
+
+const calculateAverage = function (
+    number1,
+    number2,
+    number3,
+    number4,
+    number5
+) {
+    const total = number1 + number2 + number3 + number4 + number5;
+    const average = total / 5;
+    return average;
+};
+
+console.log(calculateAverage(1, 1, 1, 1, 1)); // 1
+console.log(calculateAverage(1, 2, 3, 4, 5)); // 3
+console.log(calculateAverage(-10000, 0, 0, 0, 5000)); // -1000
+
+//--If you want to round the average you can do that like so:
+
+const calculateAverage = function (
+    number1,
+    number2,
+    number3,
+    number4,
+    number5
+) {
+    const total = number1 + number2 + number3 + number4 + number5;
+    const average = total / 5;
+    const averageRounded = Math.round(average);
+    return averageRounded;
+};
