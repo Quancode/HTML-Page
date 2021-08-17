8a055e89b128610ece88f5b60097263624ad7f99
Quanita Emanuel <77808036+Quancode@users.noreply.github.com>
2021-08-17 15:07:46 +0200

    Function return statements
    Function return statements

---
+++ Opdracht Function return statements/script.js
@@ -0,0 +1,19 @@
+
+
+const number = function (myAge) {
+    console.log(myAge);
+    return number;
+}
+
+const number = function (myAge) {
+    if (myAge >= 100) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+};
+
+const result = myAge(55);
+console.log(result);
+
+//--------
